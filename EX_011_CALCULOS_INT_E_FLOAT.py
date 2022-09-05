7ee59881ddf467ea326f6f4249398c1c98e23a42
Adilson Garcia <108878646+ADILSONLGARCIA@users.noreply.github.com>
2022-09-05 17:14:11 -0300

    Create EX_011_CALCULOS_INT_E_FLOAT.py
    011. Faça um Programa que peça 2 números inteiros e um número real.
    # Calcule e mostre:
    # o produto do dobro do primeiro com metade do segundo .
    # a soma do triplo do primeiro com o terceiro.
    # o terceiro elevado ao cubo.

---
+++ EX_011_CALCULOS_INT_E_FLOAT.py
@@ -0,0 +1,5 @@
+# 011. Faça um Programa que peça 2 números inteiros e um número real.
+# Calcule e mostre:
+# o produto do dobro do primeiro com metade do segundo .
+# a soma do triplo do primeiro com o terceiro.
+# o terceiro elevado ao cubo.
