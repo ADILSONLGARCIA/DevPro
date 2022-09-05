c52df53dc732e4ced60bea71573171cf9654868e
Adilson Garcia <108878646+ADILSONLGARCIA@users.noreply.github.com>
2022-09-05 10:21:30 -0300

    Realizado mais dois programas da lista de exercícios
    Mais dois programas:
    Conversão de medida e área de um circulo.

---
+++ EX_005_CONVERSÃO_MEDIDAS.py
@@ -0,0 +1,5 @@
+#  Faça um Programa que converta metros para centímetros.
+medidas = 0
+metros = float(input('Informe a medida em metros: '))
+centimetros = metros * 100
+print(f'Os {metros:.2f} metros informados, equivalem a {centimetros:.1f} centimetros.')
---
+++ EX_006_AREA_CIRCULO.py
@@ -0,0 +1,4 @@
+# 006. Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+raio = float(input('Qual a medida do raio? R: '))
+area = 3.14 * (raio ** 2)
+print(f'A area do circulo corresponde a {area:.2f} cm de área.')
