ea947d6f52f8d4e3a85c2a11fca55710554f10d0
Adilson Garcia <108878646+ADILSONLGARCIA@users.noreply.github.com>
2022-09-05 12:11:57 -0300

    Create EX_007_AREA_QUADRADO.py
     Programa que calcula a área de um quadrado, e em seguida mostra o dobro deste.

---
+++ EX_007_AREA_QUADRADO.py
@@ -0,0 +1,7 @@
+# 007. Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+soma = quadrado = 0
+quadrado = float(input(f'Informe a medida de um lado do quadrado: '))
+area = quadrado * 4
+dobro = area * 2
+print(f'O dobro da area {area} do quadrado é {dobro}')
+
