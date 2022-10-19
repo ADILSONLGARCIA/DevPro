43c4ea71b32a85e71e854b6a21069c82383a9bc4
Adilson Garcia <108878646+ADILSONLGARCIA@users.noreply.github.com>
2022-10-19 12:32:28 -0300

    Fatiando e contando strings
    Fatiando e contando strings

---
+++ FATIANDO_CONTANDO_STRING_DICIONARIO.py
@@ -0,0 +1,33 @@
+def contar_caracteres(s):
+    """
+    Função que conta os caracteres de uma string
+    Ex:
+    >>> contar_caracteres('renzo')
+    {'e': 1, 'n': 1, 'o': 1, 'r': 1, 'z': 1}
+    >>> contar_caracteres('banana')
+    {'a': 3, 'b': 1, 'n': 2}
+
+    :param s: string a ser contada
+    """
+    caracteres_ordenados = sorted(s)
+    caracter_anterior = caracteres_ordenados[0]
+    contagem = 1
+
+    resultado={}
+
+    for caracter in caracteres_ordenados[1:]:
+        if caracter == caracter_anterior:
+            contagem += 1
+        else:
+            resultado[caracter_anterior] = contagem
+            caracter_anterior = caracter
+            contagem = 1
+
+    resultado[caracter_anterior] = contagem
+
+    return resultado
+
+if __name__=='__main__':
+    print(contar_caracteres('renzo'))
+    print()
+    print(contar_caracteres('banana'))
